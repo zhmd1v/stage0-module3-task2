fb2e6dd3685de877a625779533e912b8146fb094
zhmd1v <zhumadelov.kostanay@gmail.com>
2022-10-06 14:42:01 +0600

    Key Points

--- src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
@@ -1,6 +1,6 @@
 package lang.print.gaps.task2;
 
-public class advancedNamingConvention {
+public class AdvancedNamingConvention {
     private static final int ADULT_AGE = 18;
 
     private int age;
--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -7,7 +7,7 @@ public class NamingConvention {
         int variableTwo = 2;
         int variableThree = 3;
         final int VARIABLE_FOUR = 4;
-        final int VARIABLE_fIVE = 5;
+        final int VARIABLE_FIVE = 5;
         int variableSix = 6;
         int variableSeven = 7;
         int variableEight = 8;
